ec558419e6482d8c19483c974ddf8ea7e8cb3855
PhoenixofMT <phoenixofmt@gmail.com>
2026-09-13 03:06:59 -0600

    Add clock.

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ def main():
 
     pg.init()
     screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pg.time.Clock()
+    dt = 0.0
 
     while True:
         log_state()
@@ -22,6 +24,6 @@ def main():
 
 
         pg.display.flip()
-
+        dt = clock.tick(60) / 1000
 if __name__ == "__main__":
     main()
