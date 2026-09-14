c11f883ef81187c250ad30df0f5ffe4f1ab529a6
PhoenixofMT <phoenixofmt@gmail.com>
2026-09-14 00:51:52 -0600

    Sort imports to make the IDE happy.

--- main.py
+++ main.py
@@ -1,29 +1,30 @@
-import pygame as pg
+import pygame
+
 from constants import *
 from logger import log_state
 
 
 def main():
-    print(f"Starting Asteroids with pygame version {pg.version.ver}")
+    print(f"Starting Asteroids with pygame version {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
-    pg.init()
-    screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    clock = pg.time.Clock()
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
     dt = 0.0
 
     while True:
         log_state()
 
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
                 return
 
         screen.fill("black")
 
 
-        pg.display.flip()
+        pygame.display.flip()
         dt = clock.tick(60) / 1000
 if __name__ == "__main__":
     main()
