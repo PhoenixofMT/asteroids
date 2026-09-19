d6fdb873de68ba54dd32fe9bf5f2e6e9a0d36249
PhoenixofMT <phoenixofmt@gmail.com>
2026-09-18 18:25:45 -0600

    Add collision processing to main.

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import sys
+
 import pygame
 
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
 from constants import *
-from logger import log_state
+from logger import log_event, log_state
 from player import Player
 
 
@@ -44,8 +46,15 @@ def main():
         screen.fill("black")
 
         updatable.update(dt)
+
         for sprite in drawable:
             sprite.draw(screen)
+
+        for asteroid in asteroids:
+            if player.collides_with(asteroid):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
 
         pygame.display.flip()
         dt = clock.tick(60) / 1000
