ee7ebc307027f4456931f8d1e3741bd8e49fca7f
PhoenixofMT <phoenixofmt@gmail.com>
2026-09-19 20:32:02 -0600

    Fix super call in Shot.

--- shot.py
+++ shot.py
@@ -6,7 +6,7 @@ from constants import LINE_WIDTH, SHOT_RADIUS
 
 class Shot(CircleShape):
     def __init__(self, x: float, y: float) -> None:
-        super().__init(x, y, SHOT_RADIUS)
+        super().__init__(x, y, SHOT_RADIUS)
 
     def draw(self, screen: pygame.Surface) -> None:
         pygame.draw.circle(screen, "white", self.position, self.radius, LINE_WIDTH)
