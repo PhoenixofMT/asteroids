12bd856e15620e4dacb82dfd94b887096d8330e3
PhoenixofMT <phoenixofmt@gmail.com>
2026-09-21 03:08:47 -0600

    Add duration to each shot. Check duration during update and kill if time runs out.

--- shot.py
+++ shot.py
@@ -1,15 +1,19 @@
 import pygame
 
 from circleshape import CircleShape
-from constants import LINE_WIDTH, SHOT_RADIUS
+from constants import LINE_WIDTH, SHOT_DURATION, SHOT_RADIUS
 
 
 class Shot(CircleShape):
     def __init__(self, x: float, y: float) -> None:
         super().__init__(x, y, SHOT_RADIUS)
+        self.duration = SHOT_DURATION
 
     def draw(self, screen: pygame.Surface) -> None:
         pygame.draw.circle(screen, "white", self.position, self.radius, LINE_WIDTH)
 
     def update(self, dt: float) -> None:
         self.position += (self.velocity * dt)
+        self.duration -= dt
+        if self.duration <= 0:
+            self.kill()
